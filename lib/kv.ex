37f505227abbcbcbf7ac0846ac4b30ebecd393cf
Alexandre Karpov <alexakarpov@gmail.com>
2018-01-17 12:58:52 -0500

    application starting with a supervisor... need to revisit this later

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,7 +1,13 @@
 defmodule KV do
+  use Application
+
   @moduledoc """
   Documentation for KV.
   """
+
+  def start(_type, _args) do
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
 
   @doc """
   Hello world.
