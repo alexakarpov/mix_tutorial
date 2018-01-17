46f2ed8c127a20a36c13af50c38557180f999a20
Alexandre Karpov <alexakarpov@gmail.com>
2018-01-16 22:28:07 -0500

    implement and test deletion

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -13,4 +13,14 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "deletes value by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    
+    assert KV.Bucket.delete(bucket, "milk") == 3
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+
 end
